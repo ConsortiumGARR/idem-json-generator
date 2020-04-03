0679d8ebe8a5f9d64fcf16e91084dcded52d19bc
Marco Malavolti <marco.malavolti@gmail.com>
2020-04-03 20:10:55 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# IDEM JSON Generator
+Small python3 script that converts Metadata XML to DiscoFeed JSON format for Shibboleth Embedded Discovery Service
+
+# Instructions
+
+* `sudo apt install python3`
+
+* `cd /opt ; sudo git clone https://git.garr.it/IDEM/idem-json-generator.git`
+
+* Configure your Apache:
+  * `sudo cp /opt/idem-json-generator/idem-json-generator.conf /etc/apache2/sites-available`
+
+* Put the following command into your preferred CRON Jobs:
+  * Choose ONE of the following command to consider only ONE metadata stream:
+    * IDEM Test Metadata: 
+      * `/usr/bin/wget http://md.idem.garr.it/metadata/idem-test-metadata-sha256.xml -O /opt/idem-json-generator/input/idem-test-metadata-sha256.xml >> /opt/idem-json-generator/wget.log 2>&1`
+    * IDEM Production Metadata:
+      * `/usr/bin/wget http://md.idem.garr.it/metadata/idem-metadata-sha256.xml -O /opt/idem-json-generator/input/idem-metadata-sha256.xml >> /opt/idem-json-generator/wget.log 2>&1`
+
+  * Use one of the following commands to generate EDS JSON file for the specific stream:
+    * IDEM Test Metadata:
+      * `/usr/bin/python3 /opt/idem-json-generator/extractDataFromMD.py -m /opt/idem-json-generator/input/idem-test-metadata-sha256.xml -o /opt/idem-json-generator/output/idem-resources.json > /opt/idem-json-generator/md-parsing.log 2>&1`
+    * IDEM Production Metadata:
+      * `/usr/bin/python3 /opt/idem-json-generator/extractDataFromMD.py -m /opt/idem-json-generator/input/idem-metadata-sha256.xml -o /opt/idem-json-generator/output/idem-resources.json > /opt/idem-json-generator/md-parsing.log 2>&1`
+
+  Example Crontab:
+  ```bash
+  20 * * * * /usr/bin/wget http://md.idem.garr.it/metadata/idem-metadata-sha256.xml -O /opt/idem-json-generator/input/idem-metadata-sha256.xml >> /opt/idem-json-generator/wget.log 2>&1
+
+  21 * * * * /usr/bin/python3 /opt/idem-json-generator/extractDataFromMD.py -m /opt/idem-json-generator/input/idem-metadata-sha256.xml -o /opt/idem-json-generator/output/idem-resources.json > /opt/idem-json-generator/md-parsing.log 2>&1
+  ```
+
+* Enable IDEM JSON Generator site:
+  * `sudo a2ensite idem-json-generator.conf`
+  * `sudo systemctl reload apache2.service`
---
+++ extractDataFromMD.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python3 
+#-*- coding: utf-8 -*-
+
+import xml.etree.cElementTree as ET
+from operator import itemgetter
+from collections import OrderedDict
+
+import sys, getopt
+import json
+
+
+# Get entityID
+def getEntityID(EntityDescriptor, namespaces):
+    return EntityDescriptor.get('entityID')
+
+
+# Get MDUI Descriptions
+def getDescriptions(EntityDescriptor,namespaces,entType='idp'):
+
+    description_list = list()
+    if (entType.lower() == 'idp'):
+       descriptions = EntityDescriptor.findall("./md:IDPSSODescriptor/md:Extensions/mdui:UIInfo/mdui:Description", namespaces)
+    if (entType.lower() == 'sp'):
+       descriptions = EntityDescriptor.findall("./md:SPSSODescriptor/md:Extensions/mdui:UIInfo/mdui:Description", namespaces)
+
+    for desc in descriptions:
+        descriptions_dict = dict()
+        descriptions_dict['value'] = desc.text
+        descriptions_dict['lang'] = desc.get("{http://www.w3.org/XML/1998/namespace}lang")
+        description_list.append(descriptions_dict)
+    
+    return description_list
+
+
+# Get MDUI Logos
+def getLogos(EntityDescriptor,namespaces,entType='idp'):
+
+    logos_list = list()
+    if (entType.lower() == 'idp'):
+       logo_urls = EntityDescriptor.findall("./md:IDPSSODescriptor/md:Extensions/mdui:UIInfo/mdui:Logo", namespaces)
+    if (entType.lower() == 'sp'):
+       logo_urls = EntityDescriptor.findall("./md:SPSSODescriptor/md:Extensions/mdui:UIInfo/mdui:Logo", namespaces)
+
+    for logo in logo_urls:
+        logo_dict = dict()
+        logo_dict['value'] = logo.text
+        logo_dict['width'] = logo.get("width")
+        logo_dict['height'] = logo.get("height")
+        logo_dict['lang'] = logo.get("{http://www.w3.org/XML/1998/namespace}lang")
+        logos_list.append(logo_dict)
+
+    return logos_list
+
+# Get ServiceName
+def getServiceName(EntityDescriptor,namespaces,lang='it'):
+    serviceName = EntityDescriptor.find("./md:SPSSODescriptor/md:AttributeConsumingService/md:ServiceName[@xml:lang='it']", namespaces)
+    if (serviceName != None):
+       return serviceName.text
+    else:
+       serviceName = EntityDescriptor.find("./md:SPSSODescriptor/md:AttributeConsumingService/md:ServiceName[@xml:lang='en']", namespaces)
+       if (serviceName != None):
+          return serviceName.text
+       else:
+          return ""
+
+
+# Get Organization Name
+def getOrgName(EntityDescriptor, namespaces):
+    orgName = EntityDescriptor.find("./md:Organization/md:OrganizationName[@xml:lang='it']",namespaces)
+
+    if (orgName != None):
+       return orgName.text
+    else:
+       orgName = EntityDescriptor.find("./md:Organization/md:OrganizationName[@xml:lang='en']",namespaces)
+       if (orgName != None):
+          return orgName.text
+       else:
+          return ""
+
+# Gen InformationURL
+def getInformationURL(EntityDescriptor,namespaces):
+    info = EntityDescriptor.find("./md:SPSSODescriptor/md:Extensions/mdui:UIInfo/mdui:InformationURL[@xml:lang='it']",namespaces)
+    if (info != None):
+       return info.text
+    else:
+       info = EntityDescriptor.find("./md:SPSSODescriptor/md:Extensions/mdui:UIInfo/mdui:InformationURL[@xml:lang='en']",namespaces)
+       if (info != None):
+          return info.text
+       else:
+          info = EntityDescriptor.find("./md:Organization/md:OrganizationURL[@xml:lang='it']",namespaces)
+          if (info != None):
+             return info.text
+          else:
+              info = EntityDescriptor.find("./md:Organization/md:OrganizationURL[@xml:lang='en']",namespaces)
+              if (info != None):
+                 return info.text
+              else:
+                 return ""
+
+
+def main(argv):
+   try:
+      # 'm:o:hd' means that 'm' and 'o' needs an argument(confirmed by ':'), while 'h' and 'd' don't need it
+      opts, args = getopt.getopt(sys.argv[1:], 'm:o:hd', ['metadata=','output=','help','debug' ])
+   except getopt.GetoptError as err:
+      print (str(err))
+      print ("Usage: ./extractDataFromMD.py -m <md_inputfile> -o <output_file>")
+      print ("The JSON content will be put in the output file")
+      sys.exit(2)
+
+   inputfile = None
+   outputfile = None
+   idp_outputfile = None
+
+   for opt, arg in opts:
+      if opt in ('-h', '--help'):
+         print ("Usage: ./extractDataFromMD.py -m <md_inputfile> -o <output_file>")
+         print ("The JSON content will be put in the output file")
+         sys.exit()
+      elif opt in ('-m', '--metadata'):
+         inputfile = arg
+      elif opt in ('-o', '--output'):
+         outputfile = arg
+      elif opt == '-d':
+         global _debug
+         _debug = 1
+      else:
+         print ("Usage: ./extractDataFromMD.py -m <md_inputfile> -o <output_file>")
+         print ("The JSON content will be put in the output file")
+         sys.exit()
+
+   namespaces = {
+      'xml':'http://www.w3.org/XML/1998/namespace',
+      'md': 'urn:oasis:names:tc:SAML:2.0:metadata',
+      'mdrpi': 'urn:oasis:names:tc:SAML:metadata:rpi',
+      'shibmd': 'urn:mace:shibboleth:metadata:1.0',
+      'mdattr': 'urn:oasis:names:tc:SAML:metadata:attribute',
+      'saml': 'urn:oasis:names:tc:SAML:2.0:assertion',
+      'ds': 'http://www.w3.org/2000/09/xmldsig#',
+      'mdui': 'urn:oasis:names:tc:SAML:metadata:ui'
+   }
+
+   if inputfile == None:
+      print ("Metadata file is missing!\n")
+      print ("Usage: ./extractDataFromMD.py -m <md_inputfile> -o <output_file>")
+      print ("The JSON content will be put in the output file")
+      sys.exit()
+
+   if outputfile == None:
+      print ("Output file is missing!\n")
+      print ("Usage: ./extractDataFromMD.py -m <md_inputfile> -o <output_file>")
+      print ("The JSON content will be put in the output file")
+      sys.exit()
+
+
+   tree = ET.parse(inputfile)
+   root = tree.getroot()
+   sp = root.findall("./md:EntityDescriptor[md:SPSSODescriptor]", namespaces)
+
+   sps = dict()
+
+   list_sps = list()
+
+   cont_id = 0
+
+   for EntityDescriptor in sp:
+
+      cont_id = cont_id + 1
+      #pp_flag = "Privacy Policy assente"
+      #info_flag = "Info Page assente"
+      #logo_flag = "Logo non presente"
+
+      # Get entityID
+      entityID = getEntityID(EntityDescriptor,namespaces)
+
+      # Get ServiceName ENG
+      serviceName = getServiceName(EntityDescriptor,namespaces,'en')
+
+      # Get MDUI Privacy Policy
+      #pp_list = getPrivacyStatementURLs(EntityDescriptor,namespaces,'idp')
+
+      #if (len(pp_list) != 0):
+      #   pp_flag = 'Privacy Policy presente'
+
+      # Get MDUI Info Page
+      infoUrl = getInformationURL(EntityDescriptor,namespaces)
+     
+      # Get MDUI Logos
+      #logos_list = getLogos(EntityDescriptor,namespaces,'sp')
+
+      #if (len(logos_list) != 0):
+      #   logo_flag = 'Logo presente'
+
+      # Get RequestedAttribute
+      reqAttr = EntityDescriptor.findall("./md:SPSSODescriptor/md:AttributeConsumingService/md:RequestedAttribute", namespaces)
+      requestedAttributes = list()
+
+      if (reqAttr != None):
+         for ra in reqAttr:
+            if(ra.get('isRequired') == "true"):
+               requestedAttributes.append(ra.get('FriendlyName')+"(O)")
+            else:
+               requestedAttributes.append(ra.get('FriendlyName')+"(R)")
+
+      #Get Organization Name
+      orgName = getOrgName(EntityDescriptor,namespaces)
+
+      sp = OrderedDict([
+        ('id',cont_id),
+        ('IdemResource_name',serviceName),
+        ('IdemResource_serviceUrlOne', infoUrl),
+        ('IdemResource_uri',', '.join(requestedAttributes)),
+        ('IdemAccession_name',orgName)
+      ])
+
+      list_sps.append(sp)
+
+   
+   result_sps = open(outputfile, "w",encoding=None)
+   result_sps.write(json.dumps(sorted(list_sps,key=itemgetter('id')),sort_keys=False, indent=4, ensure_ascii=False))
+   result_sps.close()
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
